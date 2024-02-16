d5ceab5657f80c4cc8aaca61e671e5f88286c84b
Jabi <michal.jabi@gmail.com>
2024-02-16 14:07:32 +0100

    explain(04): classes and functions (factories) - making new objects.

--- C-complex-types-using-objects-and-arrays/c04-train-objects.js
+++ C-complex-types-using-objects-and-arrays/c04-train-objects.js
@@ -38,3 +38,51 @@ console.log(basketValue);
 // # 3 Show that initializing two new empty objects after comparing === will give us false.
 // Even when they got the same properties and values (!!!)
 // Explain why this is happening?
+console.log(shoppingCartItem2 === shoppingCartItem)
+console.log({} === {})
+
+
+// Why functional approach with JS is so important?
+class ShoppingCartItem {
+	constructor(name, price, tax) {
+		this.name = name;
+		this.price = price;
+		this.tax = tax;
+	}
+}
+
+const myItem1 = new ShoppingCartItem('Sample', 200, 12);
+const myItem2 = new ShoppingCartItem('Sample2', 220, 22);
+
+console.log(myItem1)
+console.log(myItem2)
+console.log(myItem1 === myItem2)
+
+// The same might be achieved with function with less code,
+
+function makeShoppingCartItem(name, price = 100, tax = 23) {
+	return { 
+		name, 
+		price, 
+		tax 
+	}
+}
+
+const myItem3 = makeShoppingCartItem('Sample3', 200, 12);
+const myItem4 = makeShoppingCartItem('Sample4', 220, 22);
+
+console.log(myItem3)
+console.log(myItem4)
+console.log(myItem3 === myItem4)
+
+const arrayOfObj = [
+	makeShoppingCartItem('ASample1', 200, 12),
+	makeShoppingCartItem('ASample2', 200, 12),
+	makeShoppingCartItem('ASample3', 200, 12),
+	makeShoppingCartItem('ASample4', 200, 12)
+]
+
+console.log(arrayOfObj)
+
+
+arrayOfObj.push(makeShoppingCartItem('ASample5', 200, 12))
